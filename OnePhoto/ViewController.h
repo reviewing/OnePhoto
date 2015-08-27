e0fa9543c68fb978d0a0a1bb0c5e852e9bf654a6
duanhong <duanhong@baidu.com>
2015-08-27 23:06:51 +0800

    initial project

---
+++ OnePhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnePhoto
+//
+//  Created by Hong Duan on 8/27/15.
+//  Copyright (c) 2015 Hong D. Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

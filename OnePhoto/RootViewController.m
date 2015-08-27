ace743d5f295cbc2a982a86d2cb1a8788dbc99c5
duanhong <duanhong@baidu.com>
2015-08-27 23:26:38 +0800

    change name/ launch screen

--- OnePhoto/RootViewController.m
+++ OnePhoto/RootViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2015 Hong D. Empire. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "RootViewController.h"
 
-@interface ViewController ()
+@interface RootViewController ()
 
 @end
 
-@implementation ViewController
+@implementation RootViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];

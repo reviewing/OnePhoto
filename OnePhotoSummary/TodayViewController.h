6155046631eefbe8c50dc18d352e092c204b11d8
duanhong <duanhong169@gmail.com>
2015-11-04 11:28:57 +0800

    增加Today Widget

---
+++ OnePhotoSummary/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  OnePhotoSummary
+//
+//  Created by Hong Duan on 11/3/15.
+//  Copyright © 2015 Hong D. Empire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ OnePhotoSummary/TodayViewController.m
@@ -0,0 +1,42 @@
+//
+//  TodayViewController.m
+//  OnePhotoSummary
+//
+//  Created by Hong Duan on 11/3/15.
+//  Copyright © 2015 Hong D. Empire. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+- (UIEdgeInsets)widgetMarginInsetsForProposedMarginInsets:(UIEdgeInsets)defaultMarginInsets {
+    return UIEdgeInsetsMake(defaultMarginInsets.top, defaultMarginInsets.left, 0, defaultMarginInsets.right);
+}
+
+@end
